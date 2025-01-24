94b6b79e21e032cdfd85ee8a99760f78bea661c5
jhachet <111529553+jhachet@users.noreply.github.com>
2025-01-24 08:37:21 -0500

    Update get_covered_entity_daily.py

--- Python/get_covered_entity_daily.py
+++ Python/get_covered_entity_daily.py
@@ -10,12 +10,11 @@ from wait_for_download import wait_for_download
 
 def get_covered_entity_daily(download_dir, snapshot_date):
     options = Options()
-
-    browser = webdriver.Firefox(options=options)
     options.set_preference("browser.download.folderList", 2)
     options.set_preference("browser.download.manager.showWhenStarting", False)
     options.set_preference("browser.download.dir", download_dir)
-    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/json")
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/ZIP")
+    browser = webdriver.Firefox(options=options)
 
     try:
         browser.get("https://340bopais.hrsa.gov/reports")
@@ -23,16 +22,19 @@ def get_covered_entity_daily(download_dir, snapshot_date):
         button = wait.until(
             EC.element_to_be_clickable((By.ID, "ctl00_ContentPlaceHolder1_lnkCEDailyJSONDump"))
         )
-
         browser.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", button)
         button.click()
-        brower.quit()
+        print("Waiting for the download to complete...")
+        if wait_for_download(download_dir):
+            print("Download completed successfully!")
+        else:
+            print("Download did not complete within the timeout period.")
 
-        while not wait_for_download(download_dir):
-            time.sleep(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
     finally:
-            browser.quit()
+        browser.quit()
 
 
-
-
+if __name__ == "__main__":
+    get_covered_entity_daily(download_directory, snapshot_date)
